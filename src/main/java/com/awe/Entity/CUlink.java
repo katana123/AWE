6e3257077ba1196112859dba2e16ea7b9d30fd11
katana123 <664943439@qq.com>
2018-05-30 20:40:47 +0800

    添加机构成员

---
+++ src/main/java/com/awe/Entity/CUlink.java
@@ -0,0 +1,37 @@
+package com.awe.Entity;
+
+
+public class CUlink {
+
+    private long ccid;
+    private long cuid;
+    private long roleid;
+
+
+    public long getCcid() {
+        return ccid;
+    }
+
+    public void setCcid(long ccid) {
+        this.ccid = ccid;
+    }
+
+
+    public long getCuid() {
+        return cuid;
+    }
+
+    public void setCuid(long cuid) {
+        this.cuid = cuid;
+    }
+
+
+    public long getRoleid() {
+        return roleid;
+    }
+
+    public void setRoleid(long roleid) {
+        this.roleid = roleid;
+    }
+
+}
